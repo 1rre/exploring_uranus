48050dd21540d68d5a5053a4967ea34b7c21644f
Timothy Moores <info@tmoor.es>
2021-05-24 14:33:32 +0100

    line avg & not together

--- vision/quartus/software/D8M_Camera_Test_bsp/system.h
+++ vision/quartus/software/D8M_Camera_Test_bsp/system.h
@@ -4,7 +4,7 @@
  * Machine generated for CPU 'nios2_gen2' in SOPC Builder design 'Qsys'
  * SOPC Builder design path: ../../Qsys.sopcinfo
  *
- * Generated: Sun May 23 21:11:00 BST 2021
+ * Generated: Mon May 24 14:29:06 BST 2021
  */
 
 /*
@@ -464,7 +464,7 @@
 #define SYSID_QSYS_IRQ_INTERRUPT_CONTROLLER_ID -1
 #define SYSID_QSYS_NAME "/dev/sysid_qsys"
 #define SYSID_QSYS_SPAN 8
-#define SYSID_QSYS_TIMESTAMP 1621800072
+#define SYSID_QSYS_TIMESTAMP 1621862045
 #define SYSID_QSYS_TYPE "altera_avalon_sysid_qsys"
 
 
